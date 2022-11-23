6f07c27c1c11833b23c07c2862f0429851b271d8
zoltarzol <duroisin@gmail.com>
2022-11-23 16:55:29 +0100

    ajout de features a la navbar

--- features/urls.py
+++ features/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.FeaturesListView.as_view(), name='features_list_view'),
-    path('detail/<int:pk>', views.FeatureDetailsView.as_view(), name='feature_details_view'),
+    path('', views.FeaturesListView.as_view(), name='features_list'),
+    path('detail/<int:pk>', views.FeatureDetailsView.as_view(), name='feature_details'),
 ]
